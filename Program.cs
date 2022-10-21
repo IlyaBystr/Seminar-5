d4cf3a42985c43d4799b0093a1f0aa4a85e019b5
ilya <fox367213@gmail.com>
2022-10-21 14:01:00 +0300

    сделана первая задача

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+randomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArr(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+    if (numbers[i] % 2 == 0)
+    {
+        count++;
+    }
+
+
+Console.WriteLine($"{numbers.Length} чиселв массиве, чётных из них {count}");
+
+void randomNumbers(int[] numbers)
+{
+    for (int a = 0; a < numbers.Length; a++)
+    {
+        numbers[a] = new Random().Next(100, 1000);
+    }
+}
+void PrintArr(int[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine("]");
+}
